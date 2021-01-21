e4c19230d1672cee93210699019c441985e82f5d
sachin singh <48687519+sachin3018@users.noreply.github.com>
2021-01-21 14:57:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Router from './Router'
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(<Router/>,
   document.getElementById('root')
